99bc2891b7587ad1d7968f7de95a561cda010f8d
elephantMax <hatypovm12@mail.ru>
2023-12-23 22:06:40 +0300

    add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
-})
+  devtools : { enabled: true },
+  modules  : [
+    '@pinia/nuxt',
+    '@nuxtjs/eslint-module',
+  ],
+  eslint: {
+    fix: true,
+  },
+});
